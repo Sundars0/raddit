98e3749cbf960f4cec4f55edcd73a0e19acc9a84
Sundars0 <Sundars04@rediffmail.com>
2016-10-30 05:50:03 +0000

    add gem and add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :comments
   devise_for :users
   root :to => 'links#index'
   
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
       put "like",     to: "links#upvote"
       put "dislike",  to: "links#downvote"
     end
+    resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
